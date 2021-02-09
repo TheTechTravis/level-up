54d43474d1f6c852391c84532baf5921d433d246
Travis Stevenson <thetechtravis@outlook.com>
2021-02-09 14:28:32 -0600

    Ch. 7, complete

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,17 +1,23 @@
 from rest_framework import routers
-from levelupapi.views import GameTypes
+from levelupapi.views import Games, GameTypes
 from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 
+
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
+router.register(r'games', Games, 'game')
+
 
 urlpatterns = [
     path('', include(router.urls)),
+
     # Requests to http://localhost:8000/register will be routed to the register_user function
     path('register', register_user),
+
     # Requests to http://localhost:8000/login will be routed to the login_user function
     path('login', login_user),
+
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
